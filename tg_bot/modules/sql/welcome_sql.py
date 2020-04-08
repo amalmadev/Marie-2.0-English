3efbae3a23cb9af08e8f9051969d40cee55c2a54
amalmadev <57911733+amalmadev@users.noreply.github.com>
2020-04-08 21:59:17 +0530

    Update welcome_sql.py

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 from tg_bot.modules.helper_funcs.msg_types import Types
 from tg_bot.modules.sql import SESSION, BASE
 
-DEFAULT_WELCOME = "Hey {first}, how are you?"
+DEFAULT_WELCOME = "Hey {first}, We hope you'll do some amazing works here! A warm welcome and lots of good wishes on becoming part of our growing team. Congratulations and on behalf of all the members. ...Warmestwelcome!"
 DEFAULT_GOODBYE = "Nice knowing ya!"
 
 
